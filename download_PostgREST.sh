cddc1de7d573e02efd967de2662de36de30edba4
carlosescom <carlos.escom.ipn@gmail.com>
2020-05-05 13:24:03 -0500

    +echo

--- download_PostgREST.sh
+++ download_PostgREST.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 wget https://github.com/PostgREST/postgrest/releases/download/v7.0.0/postgrest-v7.0.0-ubuntu.tar.xz
 tar -xf postgrest-v7.0.0-ubuntu.tar.xz
+echo "#### PostgREST descargado con éxito ####"
 ls
