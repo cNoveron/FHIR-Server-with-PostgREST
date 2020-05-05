429b21cc2a3625485b402492ce69717b3ec4da7e
carlosescom <carlos.escom.ipn@gmail.com>
2020-05-05 13:19:20 -0500

    fix gzip: stdin: not in gzip format

--- download_PostgREST.sh
+++ download_PostgREST.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://github.com/PostgREST/postgrest/releases/download/v7.0.0/postgrest-v7.0.0-ubuntu.tar.xz
-tar -xzf postgrest-v7.0.0-ubuntu.tar.xz
+tar -xf postgrest-v7.0.0-ubuntu.tar.xz
 ls
