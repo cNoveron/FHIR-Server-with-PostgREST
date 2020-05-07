032b7ea0e3c0ce3052e4cd0d92a4d597a0acb2ce
carlosescom <carlos.escom.ipn@gmail.com>
2020-05-06 21:05:46 -0500

    - unnecesary file

--- sql/Patient_Bio.sql
+++
@@ -1,8 +0,0 @@
-alter table "Patient" add column consent boolean;
-
-alter table "Patient_Bio" drop column patient_bio_id;
-alter table "Patient_Data" drop column patient_data_id;
-alter table "Patient_Bio" add column patient_birth_date date not null;
-alter table "Patient_Bio" add column patient_id varchar(36) not null 
-    constraint "unique" unique (patient_id)
-    constraint "foreign_key" foreign key (patient_id) references "Patient"(patient_id);
