8e5d27e7c565256f6c3323d0608c19ecbee65637
Ramses Martinez <ramses.mtz96@gmail.com>
2020-05-12 13:10:24 -0500

    fix download postgrest script

--- scripts/03_download_PostgREST.sh
+++ scripts/03_download_PostgREST.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget https://github.com/PostgREST/postgrest/releases/download/v7.0.0/postgrest-v7.0.0-ubuntu.tar.xz
 echo "#### PostgREST descargado con éxito ####"
-tar -xfJ postgrest-v7.0.0-ubuntu.tar.xz
+tar -Jxvf postgrest-v7.0.0-ubuntu.tar.xz
 echo "#### PostgREST extraído con éxito ####"
-ls
+ls -l
