7473f22a242201093cede8b38fb80e46a1eadc0b
Ramses Martinez <ramses.mtz96@gmail.com>
2020-05-29 12:50:52 -0500

    Update postman requests

--- scripts-postgrest/003_r_schedule.sql
+++ scripts-postgrest/003_r_schedule.sql
@@ -1,15 +1,11 @@
 drop function if exists r_schedule;
  
-create or replace function r_schedule(
-	planning_horizon_start timestamp,
-	actor_id varchar
-)
+create or replace function r_schedule(actor_id varchar)
 	returns table(id jsonb, planning_horizon jsonb) 
 as $$ 
 	select resource->'id',resource->'planningHorizon' from schedule 
-		where to_timestamp(resource->'planningHorizon'->>'start','YYYY-MM-DD HH:MI:SS') > planning_horizon_start 
+		where to_timestamp(resource->'planningHorizon'->>'start','YYYY-MM-DD HH:MI:SS') > CURRENT_TIMESTAMP
 		and resource->'actor'->0->>'id'=actor_id;
 $$ language sql;
 
-
---select * from r_schedule('2013-12-25 09:30:01', '1');
+-- select * from r_schedule('1');
