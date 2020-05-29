d422a240b0a5dcde88b9b9eb7579d0bb843ee18d
Ramses Martinez <ramses.mtz96@gmail.com>
2020-05-28 22:40:46 -0500

    Upgrade sql scripts for web_anon permissions

--- sql/03_create_postgrest_data.sql
+++ sql/03_create_postgrest_data.sql
@@ -1,3 +1,13 @@
-create role web_anon nologin;
+do
+$do$
+begin
+   if not exists (
+      select
+      from pg_catalog.pg_roles
+      where rolname = 'web_anon') then
+      create role web_anon nologin;
+   end if;
+end
+$do$;
 
 grant usage on schema public to web_anon;
