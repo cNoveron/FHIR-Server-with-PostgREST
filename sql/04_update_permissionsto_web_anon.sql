82c94456097e14f4b68bdcc3f0ed72b9793f3b02
Ramses Martinez <ramses.mtz96@gmail.com>
2020-05-28 22:40:46 -0500

    Upgrade sql scripts for web_anon permissions

--- sql/04_update_permissionsto_web_anon.sql
+++ sql/04_update_permissionsto_web_anon.sql
@@ -1,1 +1,4 @@
 grant select on public.practitioner to web_anon;
+grant select on public.schedule to web_anon;
+grant select on public.slot to web_anon;
+grant select on public.diagnosticreport to web_anon; 
