5c7b2bb8682c6fac19d3b86850df9319b828c6b5
Ramses Martinez <ramses.mtz96@gmail.com>
2020-05-28 23:43:31 -0500

    Update sql scripts to authenticator role

---
+++ sql/05_update_permissions_to_authenticator.sql
@@ -0,0 +1,10 @@
+grant usage on schema public to authenticator;
+
+grant all on public.practitioner to authenticator;
+grant all on public.schedule to authenticator;
+grant all on public.slot to authenticator;
+grant all on public.diagnosticreport to authenticator;
+grant all on public.appointment to authenticator;
+
+
+
