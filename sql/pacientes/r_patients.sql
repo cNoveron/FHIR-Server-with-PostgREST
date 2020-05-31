a75c4d477911f1fd57b56529c5aa53624b6105eb
carlosescom <carlos.escom.ipn@gmail.com>
2020-05-31 00:09:05 -0500

    + return phone

--- sql/pacientes/r_patients.sql
+++ sql/pacientes/r_patients.sql
@@ -4,12 +4,14 @@ create or replace function r_patients()
 	returns table(
 		patient_id text,
 		patient_name text,
-		patient_surname text
+		patient_surname text,
+		patient_phone text
 	)
 as $$
 	select
 		resource ->> 'id',
 		resource -> 'name' -> 0 #> '{given}' ->> 0,
-		resource -> 'name' -> 0 #>> '{family}'
+		resource -> 'name' -> 0 #>> '{family}',
+		resource #>> '{telecom,0,value}'
 	from patient;
 $$ language sql;
