d78fc24bad4a39314608670816b1acd1e7884e78
carlosescom <carlos.escom.ipn@gmail.com>
2020-05-31 18:28:19 -0500

    scripts/dev/04_load_functions: + -h localhost

--- scripts/dev/04_load_functions.sh
+++ scripts/dev/04_load_functions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-psql -U postgres -d fhirbase_v4 < /fhirbase/sql/pacientes/r_patients.sql
-psql -U postgres -d fhirbase_v4 < /fhirbase/sql/médicos/r_practitioners.sql
+psql -h localhost -U postgres -d fhirbase_v4 < sql/pacientes/r_patients.sql
+psql -h localhost -U postgres -d fhirbase_v4 < sql/médicos/r_practitioners.sql
 
-psql -U postgres -d fhirbase_v4 < /fhirbase/sql/agenda/r_slots.sql
-psql -U postgres -d fhirbase_v4 < /fhirbase/sql/consultas/r_appointments.sql
+psql -h localhost -U postgres -d fhirbase_v4 < sql/agenda/r_slots.sql
+psql -h localhost -U postgres -d fhirbase_v4 < sql/consultas/r_appointments.sql
 
-psql -U postgres -d fhirbase_v4 < /fhirbase/sql/estudios_laboratorio/hemograma/r_diagnosticreport_blood_count.sql
-psql -U postgres -d fhirbase_v4 < /fhirbase/sql/estudios_laboratorio/urinalisis/r_diagnosticreport_urinalysis.sql
-psql -U postgres -d fhirbase_v4 < /fhirbase/sql/estudios_laboratorio/tomografía/r_diagnosticreport_imaging.sql
+psql -h localhost -U postgres -d fhirbase_v4 < sql/estudios_laboratorio/hemograma/r_diagnosticreport_blood_count.sql
+psql -h localhost -U postgres -d fhirbase_v4 < sql/estudios_laboratorio/urinalisis/r_diagnosticreport_urinalysis.sql
+psql -h localhost -U postgres -d fhirbase_v4 < sql/estudios_laboratorio/tomografía/r_diagnosticreport_imaging.sql
