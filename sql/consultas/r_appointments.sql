4dab3230a75224c12482d7fcb29816e47e057a2b
carlosescom <carlos.escom.ipn@gmail.com>
2020-05-31 00:08:46 -0500

    better return format

--- sql/consultas/r_appointments.sql
+++ sql/consultas/r_appointments.sql
@@ -2,17 +2,17 @@ drop function if exists r_appointments;
 
 create or replace function r_appointments(actor_id varchar)
 	returns table(
-		appointment_id jsonb,
-		appointment_start jsonb,
-		appointment_end jsonb,
-		appointment_specialty jsonb
+		appointment_id text,
+		appointment_start text,
+		appointment_end text,
+		appointment_specialty text
 	)
 as $$
 	select
-		resource -> 'id',
-		resource -> 'requestedPeriod' -> 0 -> 'start',
-		resource -> 'requestedPeriod' -> 0 -> 'end',
-		resource -> 'specialty' -> 0 -> 'coding' -> 0 -> 'display'
+		resource ->> 'id',
+		resource -> 'requestedPeriod' -> 0 ->> 'start',
+		resource -> 'requestedPeriod' -> 0 ->> 'end',
+		resource -> 'specialty' -> 0 -> 'coding' -> 0 ->> 'display'
 	from appointment,
 		jsonb_array_elements(resource -> 'slot') slot
 	where(
