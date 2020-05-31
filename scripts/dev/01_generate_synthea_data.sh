95bd209688a18ca430335432d70f8a139b8a6ded
carlosescom <carlos.escom.ipn@gmail.com>
2020-05-30 20:11:06 -0500

    fix echo

--- scripts/dev/01_generate_synthea_data.sh
+++ scripts/dev/01_generate_synthea_data.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "#### Running synthea build... ####"
+echo "####      Running synthea build...        ####"
 cd synthea && ./gradlew build -x test && cd ..
 
 chmod +x ./synthea/run_synthea
 
-echo "#### Running Docker container from Fhirbase image... ####"
+echo "####      Generating 50 patients...       ####"
 cd ./synthea && ./run_synthea -p 50 && cd ..
 
