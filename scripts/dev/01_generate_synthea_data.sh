fee84515bd3216807f8f5cf4939684513565e1ba
Ramses Martinez <ramses.mtz96@gmail.com>
2020-05-30 19:47:48 -0500

    add fhirbase binary

---
+++ scripts/dev/01_generate_synthea_data.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+echo "#### Running synthea build... ####"
+cd synthea && ./gradlew build -x test && cd ..
+
+chmod +x ./synthea/run_synthea
+
+echo "#### Running Docker container from Fhirbase image... ####"
+cd ./synthea && ./run_synthea -p 50 && cd ..
+
--- sql/01_create_database.sql
+++ sql/01_create_database.sql
@@ -1,5 +1,3 @@
-drop database fhirbase;
-
 create database fhirbase_v4;
 create user teeb;
 grant all privileges on database fhirbase_v4 to teeb;
