6795df5b16ea946a97dcdd5c0fa9a87b221a26fb
carlosescom <carlos.escom.ipn@gmail.com>
2020-05-31 22:44:16 -0500

    sql/agenda/r_slots: better format

--- sql/agenda/r_slots.sql
+++ sql/agenda/r_slots.sql
@@ -1,12 +1,14 @@
 drop function if exists r_slots;
 
-create or replace function r_slots(actor_id varchar)
-	returns table(
-		slot_id text,
-		slot_start text,
-		slot_end text,
-		slot_status text
-	)
+create or replace function r_slots(
+	schedule_actor_id text
+)
+returns table(
+	slot_id text,
+	slot_start text,
+	slot_end text,
+	slot_status text
+)
 as $$
 	select
 		resource ->> 'id',
@@ -21,7 +23,7 @@ as $$
 			from schedule,
 				jsonb_array_elements(resource -> 'actor') actor
 			where(
-				actor ->> 'id' = actor_id
+				actor ->> 'id' = schedule_actor_id
 			)
 		)
 	);
