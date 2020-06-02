7c9e17a8bbedc1fd5aa8dab5ee823d41655cb0f2
carlosescom <carlos.escom.ipn@gmail.com>
2020-06-02 12:26:25 -0500

    scripts/prod/05_load_functions: reordered; + slots, appointments, locations, patients functions

--- scripts/prod/05_load_functions.sh
+++ scripts/prod/05_load_functions.sh
@@ -1,15 +1,18 @@
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/pacientes/r_patients.sql
-psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/médicos/r_practitioners.sql
+< ./sql/agenda/r_slots.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/horarios/r_schedules.sql
+< ./sql/consultas/r_appointments_in_schedules_of_actor.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/agenda/r_slots.sql
+< ./sql/consultas/r_appointments_in_schedules_of_service_category.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/consultas/r_appointments.sql
+< ./sql/consultas/r_appointments_in_schedules_of_service_type.sql
+psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
+< ./sql/consultas/r_appointments_in_schedules_of_specialty.sql
+
+psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
+< ./sql/consultorios/r_locations.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
 < ./sql/estudios_laboratorio/hemograma/r_diagnosticreport_blood_count.sql
@@ -17,3 +20,14 @@ psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
 < ./sql/estudios_laboratorio/urinalisis/r_diagnosticreport_urinalysis.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
 < ./sql/estudios_laboratorio/tomografía/r_diagnosticreport_imaging.sql
+
+psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
+< ./sql/horarios/r_schedules.sql
+
+psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
+< ./sql/médicos/r_generalPractitioners_of_patient.sql
+
+psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
+< ./sql/pacientes/r_patients.sql
+psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
+< ./sql/pacientes/r_patients_of_practitioner.sql
