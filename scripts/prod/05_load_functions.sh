0b6efcb4a11e0d2ed2e79ffb3119a98075d746dc
carlosescom <carlos.escom.ipn@gmail.com>
2020-06-02 13:05:05 -0500

    scripts/prod/05_load_functions: update routes

--- scripts/prod/05_load_functions.sh
+++ scripts/prod/05_load_functions.sh
@@ -1,33 +1,33 @@
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/agenda/r_slots.sql
+< ./sql/functions/agenda/r_slots.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/consultas/r_appointments_in_schedules_of_actor.sql
+< ./sql/functions/consultas/r_appointments_in_schedules_of_actor.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/consultas/r_appointments_in_schedules_of_service_category.sql
+< ./sql/functions/consultas/r_appointments_in_schedules_of_service_category.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/consultas/r_appointments_in_schedules_of_service_type.sql
+< ./sql/functions/consultas/r_appointments_in_schedules_of_service_type.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/consultas/r_appointments_in_schedules_of_specialty.sql
+< ./sql/functions/consultas/r_appointments_in_schedules_of_specialty.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/consultorios/r_locations.sql
+< ./sql/functions/consultorios/r_locations.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/estudios_laboratorio/hemograma/r_diagnosticreport_blood_count.sql
+< ./sql/functions/estudios_laboratorio/hemograma/r_diagnosticreport_blood_count.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/estudios_laboratorio/urinalisis/r_diagnosticreport_urinalysis.sql
+< ./sql/functions/estudios_laboratorio/urinalisis/r_diagnosticreport_urinalysis.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/estudios_laboratorio/tomografía/r_diagnosticreport_imaging.sql
+< ./sql/functions/estudios_laboratorio/tomografía/r_diagnosticreport_imaging.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/horarios/r_schedules.sql
+< ./sql/functions/horarios/r_schedules.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/médicos/r_generalPractitioners_of_patient.sql
+< ./sql/functions/médicos/r_generalPractitioners_of_patient.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/pacientes/r_patients.sql
+< ./sql/functions/pacientes/r_patients.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
-< ./sql/pacientes/r_patients_of_practitioner.sql
+< ./sql/functions/pacientes/r_patients_of_practitioner.sql
