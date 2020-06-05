d34fd48ecc553e9701f1a48e7637d7043ed124ff
Ramses Martinez <ramses.mtz96@gmail.com>
2020-06-04 19:03:23 -0500

    Update scripts

--- scripts/dev/02_initialize_database.sh
+++ scripts/dev/02_initialize_database.sh
@@ -22,12 +22,3 @@ psql -d fhir_db -U postgres -p 5432 < /fhirbase/sql/04_update_permissions_to_web
 
 echo "#### Updating authenticator permissions... ####"
 psql -d fhir_db -U postgres -p 5432 < /fhirbase/sql/05_update_permissions_to_authenticator.sql
-
-echo "#### Loading postgrest functions... ####"
-psql -d fhir_db -U postgres -p 5432 < /fhirbase/scripts-postgrest/001_r_consultations.sql
-psql -d fhir_db -U postgres -p 5432 < /fhirbase/scripts-postgrest/002_get_id_from_diagnostic_report.sql
-psql -d fhir_db -U postgres -p 5432 < /fhirbase/scripts-postgrest/003_r_schedule.sql
-psql -d fhir_db -U postgres -p 5432 < /fhirbase/scripts-postgrest/004_r_slot.sql
-psql -d fhir_db -U postgres -p 5432 < /fhirbase/scripts-postgrest/005_r_ece_lab_display_metadata_blood_count.sql
-psql -d fhir_db -U postgres -p 5432 < /fhirbase/scripts-postgrest/006_r_ece_lab_display_metadata_urinalysis.sql
-psql -d fhir_db -U postgres -p 5432 < /fhirbase/scripts-postgrest/007_r_ece_lab_display_metadata_tomography.sql
--- sql/02_alter_fhir_tables.sql
+++ sql/02_alter_fhir_tables.sql
@@ -1,18 +1,12 @@
-alter table patient alter column id type uuid using id :: uuid;
-
 alter table patient alter column txid set default 0;
 
 alter table patient alter column status set default 'created';
 
 
-alter table practitioner alter column id type uuid using id :: uuid;
-
 alter table practitioner alter column txid set default 0;
 
 alter table practitioner alter column status set default 'created';
 
-
-alter table diagnosticreport alter column id type uuid using id :: uuid;
 
 alter table diagnosticreport alter column txid set default 0;
 
