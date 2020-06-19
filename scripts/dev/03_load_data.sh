2f8b870372043e5382e0b53d61cdb05ce02b3293
Ramses Martinez <ramses.mtz96@gmail.com>
2020-06-19 04:57:39 -0500

    Update scripts

--- scripts/dev/03_load_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "####    Loading data into DB using Fhirbase...    ####"
-
-fhirbase -d fhir_db load --mode=insert /fhirbase/synthea/output/fhir/*
-fhirbase -d fhir_db load --mode=insert /fhirbase/data/*
--- scripts/dev/03_load_functions.sh
+++ scripts/dev/03_load_functions.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 set -e
 
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/agenda/r_slots.sql
 
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/consultas/r_appointments_by_actor.sql
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/consultas/r_appointments_by_serviceCategory.sql
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/consultas/r_appointments_by_serviceType.sql
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/consultas/r_appointments_by_specialty.sql
 
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/consultorios/r_locations.sql
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/consultorios/r_locations_by_practitioner.sql
 
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/estudios_laboratorio/hemograma/r_diagnosticreport_blood_count.sql
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/estudios_laboratorio/urinalisis/r_diagnosticreport_urinalysis.sql
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/estudios_laboratorio/tomografía/r_diagnosticreport_imaging.sql
 
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/horarios/r_schedules.sql
 
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/médicos/r_generalPractitioners_of_patient.sql
 
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/pacientes/r_patients.sql
-psql -h localhost -p 5435 -U postgres -d fhir_db \
+psql -h localhost -p 5435 -U teeb -d fhir_db \
 < ./sql/functions/pacientes/r_patients_of_practitioner.sql
