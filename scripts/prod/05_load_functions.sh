557dfca4ba4298f2d8db26069fed118a0ff58599
carlosescom <carlos.escom.ipn@gmail.com>
2020-06-25 20:51:39 -0500

    scripts/prod/05_load_functions: +practitioner_by_location,serviceType,specialty

--- scripts/prod/05_load_functions.sh
+++ scripts/prod/05_load_functions.sh
@@ -26,8 +26,16 @@ psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
 < ./sql/functions/horarios/r_schedules_by_actor.sql
 
+psql -h localhost -p 5435 -U postgres -d fhir_db \
+< ./sql/functions/médicos/r_practitioners.sql
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
 < ./sql/functions/médicos/r_generalPractitioners_of_patient.sql
+psql -h localhost -p 5435 -U postgres -d fhir_db \
+< ./sql/functions/médicos/r_practitioners_by_location.sql
+psql -h localhost -p 5435 -U postgres -d fhir_db \
+< ./sql/functions/médicos/r_practitioners_by_serviceType.sql
+psql -h localhost -p 5435 -U postgres -d fhir_db \
+< ./sql/functions/médicos/r_practitioners_by_specialty.sql
 
 psql "postgres://teeb:$1@119.8.11.33:5432/fhirbase_v4" \
 < ./sql/functions/pacientes/r_patients.sql
