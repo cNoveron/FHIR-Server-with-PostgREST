c6e9bfe017f14651d996238f803d8ed8e8c9e96d
carlosescom <carlos.escom.ipn@gmail.com>
2020-06-29 16:18:34 -0500

    sql/example/practitioner_name_by_chargeitem_note: create

---
+++ sql/example/practitioner_name_by_chargeitem_note.sql
@@ -0,0 +1,30 @@
+drop function if exists practitioner_name_by_chargeitem_note;
+
+create or replace function practitioner_name_by_chargeitem_note(
+    chargeitem_note text
+)
+returns table(
+    practitioner_name text
+)
+as $$
+	select
+		resource #> '{name}'
+	from practitioner
+	where(
+		practitioner.resource ->> 'id' in(
+			select
+				resource #>> '{practitioner,id}'
+			from practitionerrole
+			where(
+				practitionerrole.resource ->> 'id' in(
+					select
+						resource #>> '{performer,0,actor,id}'
+					from chargeitem
+					where(
+						chargeitem.resource @> ('{"note":"'||chargeitem_note||'"}')::jsonb
+					)
+				)
+			)
+		)
+	);
+$$ language sql;
