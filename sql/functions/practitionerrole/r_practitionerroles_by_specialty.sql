ad82d3f8392a2a0cb50d6c3566fe81b1cd1c83ba
carlosescom <carlos.escom.ipn@gmail.com>
2020-06-29 17:14:33 -0500

    fix "details": "Token \"coding\" is invalid.",     "code": "22P02",     "message": "invalid input syntax for type json"

--- sql/functions/practitionerrole/r_practitionerroles_by_specialty.sql
+++ sql/functions/practitionerrole/r_practitionerroles_by_specialty.sql
@@ -17,6 +17,6 @@ as $$
 		resource #> '{telecom}'
 	from practitionerrole
     where (
-		resource @> ('{"specialty":"[{"coding":[{"code":"'||specialty_code||'"}]}]}')::jsonb
+		resource @> ('{"specialty":[{"coding":[{"code":"'||specialty_code||'"}]}]}')::jsonb
     );
 $$ language sql;
