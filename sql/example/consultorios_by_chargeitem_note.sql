aad572fe77994191a9e2e669e2cc1490a2371147
carlosescom <carlos.escom.ipn@gmail.com>
2020-06-29 21:44:29 -0500

    sql/example/consultorios_by_chargeitem_note: create

---
+++ sql/example/consultorios_by_chargeitem_note.sql
@@ -0,0 +1,29 @@
+drop function if exists consultorios_by_chargeitem_note;
+
+create or replace function consultorios_by_chargeitem_note(
+    chargeitem_note text
+)
+returns table(
+    practitionerrole_id text,
+    practitioner_name text,
+    practitionerrole_availableTime jsonb,
+    practitionerrole_location text,
+	practitionerrole_telecom jsonb,
+	serviceType_code text,
+	practitionerrole_base_appointment_price jsonb
+)
+as $$
+	select
+		practitionerrole.resource ->> 'id',
+		practitionerrole.resource #>> '{practitioner,display}',
+		practitionerrole.resource #> '{availableTime}',
+		practitionerrole.resource #>> '{location,0,display}',
+		practitionerrole.resource #> '{telecom}',
+		chargeitem.resource #>> '{code,display}',
+		chargeitem.resource #> '{priceOverride}'
+	from practitionerrole inner join chargeitem
+	on practitionerrole.resource #>> '{id}' = chargeitem.resource #>> '{performer,0,actor,id}'
+	where(
+		chargeitem.resource @> ('{"note":[{"text":"'||chargeitem_note||'"}]}')::jsonb
+	);
+$$ language sql;
