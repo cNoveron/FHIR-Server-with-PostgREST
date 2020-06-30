fba9e35e64e4d59f0c696c9801de74274bbc3ef1
carlosescom <carlos.escom.ipn@gmail.com>
2020-06-30 13:04:37 -0500

    - old functions

--- sql/example/002_get_id_from_diagnostic_report.sql
+++
@@ -1,8 +0,0 @@
-drop function if exists get_id_from_diagnosticreport;
- 
-create or replace function get_id_from_diagnosticreport(code varchar)
-returns setof jsonb as
-$$
-select resource->'id' from diagnosticreport
-where (resource->'category'->0->'coding'->0->>'code' = code);
-$$ language sql;
