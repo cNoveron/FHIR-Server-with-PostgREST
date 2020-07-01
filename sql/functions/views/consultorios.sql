c9aa4f4a47097fde57cf61dfccef305ffaaa686c
carlosescom <carlos.escom.ipn@gmail.com>
2020-07-01 11:43:44 -0500

    reorganized directories and sql/functions/views: + consultorios, consultorios_filtrados.sql

---
+++ sql/functions/views/consultorios.sql
@@ -0,0 +1,43 @@
+drop function if exists consultorios;
+
+create or replace function consultorios(
+    chargeitem_note text,
+	organization_id text,
+	specialty_code text,
+	practitioner_name_string text,
+	location_name_string text
+)
+returns table(
+    practitionerrole_id text,
+    practitioner_name text,
+    practitionerrole_availableTime jsonb,
+    practitionerrole_location text,
+	practitionerrole_telecom jsonb,
+	serviceType_code jsonb,
+	practitionerrole_base_appointment_price jsonb
+)
+as $$
+begin
+	return query select
+		practitionerrole.resource ->> 'id',
+		practitionerrole.resource #>> '{practitioner,display}',
+		practitionerrole.resource #> '{availableTime}',
+		practitionerrole.resource #>> '{location,0,id}',
+		practitionerrole.resource #> '{telecom}',
+		chargeitem.resource #> '{code,coding}',
+		chargeitem.resource #> '{priceOverride}'
+	from practitionerrole inner join chargeitem
+	on practitionerrole.resource #>> '{id}' = chargeitem.resource #>> '{performer,0,actor,id}'
+	where(
+		chargeitem.resource @> ('{"note":[{"text":"'||chargeitem_note||'"}]}')::jsonb
+		and
+		practitionerrole.resource @> ('{"organization":{"id":"'||organization_id||'"}}')::jsonb
+		and
+		practitionerrole.resource @> ('{"specialty":[{"coding":[{"code":"'||specialty_code||'"}]}]}')::jsonb
+		and
+		practitionerrole.resource @> ('{"practitioner":{"dislpay":"'||practitioner_name_string||'"}}')::jsonb
+		and
+		practitionerrole.resource @> ('{"location":[{"dislpay":"'||location_name_string||'"}]}')::jsonb
+	);
+end;
+$$ language 'plpgsql';
---
+++ sql/functions/views/consultorios_filtrados.sql
@@ -0,0 +1,41 @@
+drop function if exists consultorios_filtrados;
+
+create or replace function consultorios_filtrados(
+    chargeitem_note text,
+	organization_id text,
+	specialty_code text,
+	practitioner_name_string text,
+	location_name_string text
+)
+returns table(
+    practitionerrole_id text,
+    practitioner_name text,
+    practitionerrole_availableTime jsonb,
+    practitionerrole_location text,
+	practitionerrole_telecom jsonb,
+	serviceType_code jsonb,
+	practitionerrole_base_appointment_price jsonb,
+	healthcareservice_type jsonb
+)
+as $$
+begin
+	return query select
+		consultorios.practitionerrole_id,
+		consultorios.practitioner_name,
+		consultorios.practitionerrole_availableTime,
+		consultorios.practitionerrole_location,
+		consultorios.practitionerrole_telecom,
+		consultorios.serviceType_code,
+		consultorios.practitionerrole_base_appointment_price,
+		healthcareservice.resource #> '{type}'
+	from consultorios(
+			chargeitem_note,
+			organization_id,
+			specialty_code,
+			practitioner_name_string,
+			location_name_string
+		) as consultorios
+		inner join healthcareservice
+	on healthcareservice.resource #>> '{location,0,id}' = consultorios.practitionerrole_location;
+end;
+$$ language 'plpgsql';
