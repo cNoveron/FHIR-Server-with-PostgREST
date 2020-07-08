75f67b1191a89455e93dcbd7594fd305f8fd8a65
carlosescom <carlos.escom.ipn@gmail.com>
2020-07-08 00:13:31 -0500

    sql/example/todays_slots_for_actor: create

---
+++ sql/example/todays_slots_for_actor.sql
@@ -0,0 +1,33 @@
+drop function if exists todays_slots_for_actor;
+
+create or replace function todays_slots_for_actor(
+	actor_id text
+)
+returns table(
+	slot_id text,
+	slot_start text,
+	slot_end text,
+	slot_status text
+)
+as $$
+	select
+		slot.resource ->> 'id',
+		slot.resource ->> 'start',
+		slot.resource ->> 'end',
+		slot.resource ->> 'status'
+	from slot
+	where(
+		slot.resource #>> '{schedule,id}' in(
+			select
+				resource ->> 'id'
+			from schedule
+			where(
+				schedule.resource #>> '{actor}' = actor_id
+				and
+				to_timestamp(slot.resource #>> '{start}', 'YYYY-MM-DD HH:MI:SS') > CURRENT_TIMESTAMP
+				and
+				to_date(slot.resource #>> '{start}', 'YYYY-MM-DD HH:MI:SS') < CURRENT_DATE + '1 day'::interval
+			)
+		)
+	);
+$$ language sql;
