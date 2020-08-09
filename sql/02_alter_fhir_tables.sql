aebcd86eed01b068099f5e9bed4f03a0b643e797
carlosescom <carlos.escom.ipn@gmail.com>
2020-08-09 18:50:21 -0500

    sql: delete old scripts

--- sql/02_alter_fhir_tables.sql
+++
@@ -1,5 +0,0 @@
-alter table patient alter column status set default 'created';
-
-alter table practitioner alter column status set default 'created';
-
-alter table diagnosticreport alter column status set default 'created';
